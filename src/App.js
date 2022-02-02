984c343e46c3dc0cd77fde1cc2208598ac998a8f
iamshack <mariocore77@gmail.com>
2022-02-02 17:47:23 -0600

    changed app text2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Cambio con Robincito branch feature
+          Cambio con Robincito, branch feature
         </a>
       </header>
     </div>
